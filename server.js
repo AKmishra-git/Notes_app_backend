262f285a8423622c6c82eada842ddfe400ab7baa
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-04-22 22:39:04 +0530

    correct origin

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-import dotenv from "dotenv"
-import app from "./src/app.js"
-import connectToDB from "./src/config/database.js"
+import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectToDB from "./src/config/database.js";
 
 dotenv.config();
 
-connectToDB()
-app.listen(8000, ()=>{
-    console.log("server started");
-})
+const PORT = process.env.PORT || 8000;
 
+connectToDB()
+  .then(() => {
+    console.log("DB connected");
+
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });
 
--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-  origin: "http://localhost:3000", // your frontend URL
+  origin: ["http://localhost:3000",
+    "https://crudalgo.netlify.app/"
+  ],
+   
   credentials: true
 }))
 
