48d6b5e4c14f67f1a6f7f920e27b867225b865a7
Ayush Kumar Mishra <ak_mishra_mac@Mac.lan>
2026-04-22 22:43:26 +0530

    corrected origin 2

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: ["http://localhost:3000",
-    "https://crudalgo.netlify.app/"
+    "https://crudalgo.netlify.app"
   ],
    
   credentials: true
